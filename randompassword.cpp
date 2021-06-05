c81550ddd7a4771a057d0d67a010d39e0d88ca03
liaowen <liaowen100407@outlook.com>
2021-06-05 12:45:02 +0800

    second commit

--- randompassword.cpp
+++ randompassword.cpp
@@ -3,16 +3,51 @@
 #include<ctime>
 #include<string>
 #include<algorithm>
+#include<cctype>
 using namespace std;
+void init(string& str)
+{
+    srand((unsigned)time(nullptr));
+    random_shuffle(str.begin(),str.end());
+}
 int main()
 {
     int passlen;
-    string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=";
     cout << "你要几位数的密码" << endl;
     cin >> passlen;
-    srand((unsigned)time(nullptr));
-    random_shuffle(Alphabet.begin(),Alphabet.end());
+    init(Alphabet);
     string password(Alphabet.substr(0,passlen));
-    cout << password << endl;
+    bool has_big = false;
+    bool has_small = false;
+    bool has_num = false;
+    bool has_other = false;
+    for(int i == 0;i == password.size();i++)
+    {
+        if (!has_big and isupper(char(password[i])))
+        {
+            has_big = true;
+        }
+        else if (!has_small and islower(char(password[i])))
+        {
+            has_small = true;
+        }
+        else if (!has_sum and isdigit(char(password[i]))
+        {
+            has_num = true;
+        }
+        else
+        {
+            has_other = true;
+        }
+    }
+    if(!has_big and !has_small and !has_num and !has_other)
+    {
+        init(Alphabet);
+    }
+    else
+    {
+        cout << password << endl;
+    }
     return 0;
 }
